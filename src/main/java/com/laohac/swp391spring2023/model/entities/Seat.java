2b1c11a8e7c560d34b47c560ca4de07b25d00f8d
obito <simnhankid13042002@gmail.com>
2023-02-20 02:05:13 +0700

    flow booking

--- src/main/java/com/laohac/swp391spring2023/model/entities/Seat.java
+++ src/main/java/com/laohac/swp391spring2023/model/entities/Seat.java
@@ -1,7 +1,5 @@
 package com.laohac.swp391spring2023.model.entities;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,9 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,27 +21,18 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "tripinfor")
-public class Trip {
+@Table (name = "seat")
+public class Seat {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "TripID")
+    @Column(name = "id")
     private int id;
-    
-    @ManyToOne
-    @JoinColumn(name = "RouteID")
-    private Route route;
-    @Column(name = "StartTime")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime startTime;
-    @Column(name = "EndTime")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime endTime;
+    @Column(name = "SeatNumber")
+    private int seatNumber;
+
     @ManyToOne
     @JoinColumn(name = "CarID")
     private Car car;
-    private int price;
-
-    private String departureDetail;
-    private String arrivalDetail;
+    
 }

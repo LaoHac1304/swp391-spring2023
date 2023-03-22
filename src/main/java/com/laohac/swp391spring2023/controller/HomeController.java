9c7f86da78cad8ddc6bdaaeedc2c6f873f281f75
sonaincry <tranbaosonga@gmail.com>
2023-03-23 00:38:14 +0700

    edit database and add more trip

--- src/main/java/com/laohac/swp391spring2023/controller/HomeController.java
+++ src/main/java/com/laohac/swp391spring2023/controller/HomeController.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.laohac.swp391spring2023.model.dto.RouteDTORequest;
